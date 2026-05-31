e9e861b83bbdfc24f49df3a6e2b777254a39df8b
Adrian Smith <smith.adriane@gmail.com>
2026-05-31 10:43:06 -0700

    Support compiling with graal native image.

--- src/com/phronemophobic/automerge/impl/raw.clj
+++ src/com/phronemophobic/automerge/impl/raw.clj
@@ -31,7 +31,7 @@
             *out* w]
     (pr obj)))
 
-(def default-arguments
+(def ^:private default-arguments
   ["-resource-dir"
    "/opt/local/libexec/llvm-22/lib/clang/22"
    "-isysroot"
@@ -110,23 +110,110 @@
                                            (range 24)))))))
                   structs))))
 
+
+(def ^:private fn-names
+  #{"AMactorIdBytes"
+    "AMactorIdFromBytes"
+    "AMclone"
+    "AMcommit"
+    "AMcreate"
+    "AMemptyChange"
+    "AMequal"
+    "AMfork"
+    "AMgenerateSyncMessage"
+    "AMgetActorId"
+    "AMitemObjId"
+    "AMitemToActorId"
+    "AMitemToBool"
+    "AMitemToBytes"
+    "AMitemToCounter"
+    "AMitemToDoc"
+    "AMitemToF64"
+    "AMitemToInt"
+    "AMitemToStr"
+    "AMitemToSyncMessage"
+    "AMitemToSyncState"
+    "AMitemToTimestamp"
+    "AMitemToUint"
+    "AMitemValType"
+    "AMitemsNext"
+    "AMkeys"
+    "AMlistGet"
+    "AMlistPutBool"
+    "AMlistPutBytes"
+    "AMlistPutCounter"
+    "AMlistPutF64"
+    "AMlistPutInt"
+    "AMlistPutNull"
+    "AMlistPutObject"
+    "AMlistPutStr"
+    "AMlistPutTimestamp"
+    "AMload"
+    "AMmapGet"
+    "AMmapPutBool"
+    "AMmapPutBytes"
+    "AMmapPutCounter"
+    "AMmapPutF64"
+    "AMmapPutInt"
+    "AMmapPutNull"
+    "AMmapPutObject"
+    "AMmapPutStr"
+    "AMmapPutTimestamp"
+    "AMmerge"
+    "AMobjItems"
+    "AMobjObjType"
+    "AMobjSize"
+    "AMreceiveSyncMessage"
+    "AMresultError"
+    "AMresultFree"
+    "AMresultItem"
+    "AMresultItems"
+    "AMresultStatus"
+    "AMsave"
+    "AMsetActorId"
+    "AMstr"
+    "AMsyncMessageDecode"
+    "AMsyncMessageEncode"
+    "AMsyncStateInit"
+    "AMsyncStateDecode"
+    "AMsyncStateEncode"})
+
+
 (def api (-> (load-api)
+             (update :functions
+                     (fn [fns]
+                       (into []
+                             (filter (fn [f]
+                                       (contains? fn-names (:symbol f))))
+                             fns)))
              (adjust-amitems)))
 
-(def dtype-api (gen/api->library-interface api))
+(def dtype-api
+  (gen/api->library-interface
+   ;; graal native image doesn't allow struct by value arguments or return values
+   ;; we'll use libffi to call these functions
+   (graal-native/if-defined-graal-native
+    (update api :functions (fn [fns]
+                             (into []
+                                   (remove (fn [f]
+                                             (or(#{:clong/AMitems
+                                                   :clong/AMbyteSpan}
+                                                 (:function/ret f))
+                                                (some (fn [arg]
+                                                        (#{:clong/AMitems
+                                                           :clong/AMbyteSpan}
+                                                         arg))
+                                                      (:function/args f)))))
+                                   fns)))
+    api)))
+
 (def dtype-structs (gen/api->structs api))
 (doseq [[id fields] dtype-structs]
   (dt-struct/define-datatype! id fields))
 
-(dt-ffi/define-library-interface dtype-api
-                                 ;; :symbols
-                                 ;; '#{AMsplice}
-  :libraries
-  (graal-native/if-defined-graal-native
-   []
-   ["automerge"]))
-
-
+(dt-ffi/define-library-interface 
+ dtype-api
+ :libraries ["automerge"])
 
 (gen/def-enums api)
 
@@ -179,3 +266,63 @@
    (map (fn [[k v]]
           [v (int k)]))
    obj-type->kw))
+
+
+
+;; native image ffi does not support struct by value return types
+;; but libffi does!
+(graal-native/if-defined-graal-native
+ (require '[com.phronemophobic.clj-libffi :as ffi])
+ nil)
+
+(defn dtype-ffi-fn->cljlibffi-code [fn-name {:keys [rettype argtypes doc]}]
+  (let [argnames (into []
+                       (map first)
+                       argtypes)
+        
+        ->ffi-type (fn [arg-type]
+                     (if (and (list? arg-type)
+                              (= 'by-value (first arg-type)))
+                       (second arg-type)
+                       arg-type))
+        ->ffi-arg (fn [arg-type arg-sym]
+                    (if (= arg-type :pointer?)
+                      `(if-let [x# ~arg-sym]
+                         x#
+                         (tech.v3.datatype.ffi.Pointer. 0))
+                      arg-sym))]
+    `(defn ~(symbol (name fn-name)) 
+       ~@(when (seq doc)
+           [doc])
+       [~@argnames]
+       (ffi/call ~(name fn-name) ~(->ffi-type rettype)
+                 ~@(eduction
+                    (mapcat
+                     (fn [[arg-sym arg-type]]
+                       [(->ffi-type arg-type) 
+                        (->ffi-arg arg-type arg-sym)]))
+                    argtypes)))))
+
+
+(def libffi-api-functions 
+  (graal-native/if-defined-graal-native
+   (gen/api->library-interface 
+    {:functions
+     (into []
+           (filter (fn [f]
+                     (or (#{:clong/AMitems
+                            :clong/AMbyteSpan}
+                          (:function/ret f))
+                         (some (fn [arg]
+                                 (#{:clong/AMitems
+                                    :clong/AMbyteSpan}
+                                  arg))
+                               (:function/args f)))))
+           (:functions api))})
+   nil))
+
+(doseq [[fn-name fn-def] libffi-api-functions]
+  (let [code (dtype-ffi-fn->cljlibffi-code fn-name fn-def)]
+    (eval code)))
+
+
